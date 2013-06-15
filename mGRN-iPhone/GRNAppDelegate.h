4699acb4e5b4b678e1cf64014b12b12c4e299e4a
amian88 <anu_s_m@hotmail.com>
2013-06-15 20:13:10 +0100

    Initial Commit

---
+++ mGRN-iPhone/GRNAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  GRNAppDelegate.h
+//  mGRN-iPhone
+//
+//  Created by Anum on 15/06/2013.
+//  Copyright (c) 2013 Anum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GRNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ mGRN-iPhone/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  mGRN-iPhone
+//
+//  Created by Anum on 15/06/2013.
+//  Copyright (c) 2013 Anum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GRNAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GRNAppDelegate class]));
+    }
+}
